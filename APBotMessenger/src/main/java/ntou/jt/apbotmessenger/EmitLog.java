52648506553c6253a9cbd9600983cf2e70861a2a
jimting <handsome841206@gmail.com>
2019-04-25 15:55:24 +0800

    finally this type can push messenge to my hubot \O/

---
+++ APBotMessenger/src/main/java/ntou/jt/apbotmessenger/EmitLog.java
@@ -0,0 +1,42 @@
+package ntou.jt.apbotmessenger;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class EmitLog {
+
+	private static final String EXCHANGE_NAME = "exchangeString";
+    
+   	public static boolean send(String content)
+	{
+   		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("140.121.196.23");
+		factory.setPort(4111);
+		try (Connection connection = factory.newConnection();
+	             	Channel channel = connection.createChannel()) {
+	           	channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+			channel.basicPublish(EXCHANGE_NAME, "", null, content.getBytes("UTF-8"));
+	           	System.out.println(" [x] Sent '" + content + "'");
+			return true;
+	        } catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (TimeoutException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		return false;
+	}
+
+   	 public static void main(String[] args)
+   	 {
+		send("Hello!");
+   	 }	
+
+}
+
+
---
+++ APBotMessenger/src/test/java/ntou/jt/apbotmessenger/APBotMessengerPublisherTest.java
@@ -0,0 +1,18 @@
+package ntou.jt.apbotmessenger;
+
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+import hudson.model.Label;
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+public class APBotMessengerPublisherTest {
+
+    @Rule
+    public JenkinsRule jenkins = new JenkinsRule();
+
+}
